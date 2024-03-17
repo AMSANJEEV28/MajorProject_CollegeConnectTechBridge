33e8cd28279376fcf4e455e270864d58766b0e10
A M SANJEEV <amsanjeev28@gmail.com>
2024-03-17 21:28:58 +0530

    Event

--- events/models.py
+++ events/models.py
@@ -32,12 +32,16 @@
 #     created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 #     created_at = models.DateTimeField(auto_now_add=True)
 
+#     participants = models.ManyToManyField(CustomUser, related_name='participated_events', blank=True)
 #     def __str__(self):
 #         return self.title
 
 
+
 from django.db import models
+from django.core.exceptions import ValidationError
 from user.models import CustomUser
+from django.utils import timezone
 
 class CollegeEvent(models.Model):
     CATEGORY_CHOICES = [
@@ -71,5 +75,16 @@ class CollegeEvent(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     participants = models.ManyToManyField(CustomUser, related_name='participated_events', blank=True)
+
+    def clean(self):
+        super().clean()
+        if self.registration_deadline and self.date_and_time:
+            if self.registration_deadline >= self.date_and_time:
+                raise ValidationError("Registration deadline must be before the event date and time.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.title
